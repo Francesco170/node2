e057c1af5ab8e46f033105ed8c073fdf0fcfa102
francesco <fran.calabrese03@gmail.com>
2023-05-19 01:26:37 +0200

    esercizio11

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ const app= express();
 const port = 3000;
 
 app.use(morgan("dev"));
+app.use(express.json());
 
 
 
@@ -31,6 +32,40 @@ app.get("/api/planets/:id", (req,res)=> {
    const planet = planets.find(p => p.id === Number(id))
     res.status(200).json(planet);
 })
+
+
+app.post("/api/planets", (req,res)=>{
+  const {id, name} = req.body;
+  const newPlanet = {id ,name};
+  planets= [...planets, newPlanet];
+
+  console.log(planets);
+
+  res.status(201).json({msg:"the planet was created"});
+});
+
+app.put("/api/planets/:id", (req,res)=> {
+  const { id } = req.params;
+  const{name} = req.body
+  planets = planets.map(p => p.id === Number(id) ? ({...p,name}) :p )
+  console.log(planets)
+
+  res.status(200).json({ msg: "the planet was updated"})
+})
+
+
+
+
+app.delete("/api/planets/:id", (req,res)=> {
+  const {id} = req.params;
+  planets = planets.filter((p) => p.id !== Number(id));
+    console.log(planets)
+  res.status(200).json({msg:"thee planet was deleted"});
+})
+
+
+
+
 
 
 app.listen(port, () => {
